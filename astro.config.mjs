846812a624a37ceae125a7b1b53502d2e850627d
haruna <kharuna0404@gmail.com>
2024-03-07 00:16:57 +0900

    ssrを有効にする

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
+import nodejs from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon()],
+  output: 'server',
+  adapter: nodejs()
 });
