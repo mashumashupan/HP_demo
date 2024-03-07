00f8f9ccc09e1ed1ae717e8b1a97602bf3969065
haruna <kharuna0404@gmail.com>
2024-03-07 15:17:16 +0000

    CMS追加

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -1,11 +1,14 @@
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
-import nodejs from "@astrojs/node";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon()],
-  output: 'server',
-  adapter: nodejs()
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
 });
